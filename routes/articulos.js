398099fb51a93983d3de5815a989d47d98c5cf8e
LuisG00 <gutierrezluis6645@gmail.com>
2021-10-08 12:50:00 -0500

    modificacion de endpoint

--- routes/articulos.js
+++ routes/articulos.js
@@ -5,22 +5,48 @@ const router = express.Router()
 router.get('/', (req, res) => {
   res.json([
     {
-      name: 'Producto A',
-      price: 1000
+      name: 'Transportadora para Perros',
+      price: 500
     },
     {
-      name: 'Producto B',
-      price: 1500
+      name: 'Correa para Perros',
+      price: 35
+    },
+    {
+      name: 'Comida para gatos marca Purina catchow Adultos 3kg',
+      price: 229
+    },
+    {
+      name: 'Arena para gatos marca Scoop Away 11.6kg',
+      price: 360
+    },
+    {
+      name: 'Jaula para aves pequeñas',
+      price: 400
+    },
+    {
+      name: 'Spray antipulgas para Perros 250ml',
+      price: 95
     }
+
   ])
 })
 
-router.get('/filter', (req, res) => {
-  res.json(
+router.get('/perros', (req, res) => {
+  res.json([
     {
-      name: 'Producto A',
-      price: 1000
-    })
+      name: 'Trasportadora para Perros',
+      price: 500
+    },
+    {
+      name: 'Correa para Perros',
+      price: 35
+    },
+    {
+      name: 'Spray antipulgas para Perros 250ml',
+      price: 95
+    }
+  ])
 })
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 const itemsRoute= require('./articulos')
-const customersRoute= require('./clientes')
-
-
+//const customersRoute= require('./clientes')
+//const adoptionRoute= require('./adopcion')
+//const suppliersRoute= require('./proveedores')
+//const servicesRoute= require('./servicios')
 
 function routerApi(app){
 app.use('/articulos', itemsRoute)
-app.use('/clientes', customersRoute)
-
+//app.use('/clientes', customersRoute)
+//app.use('/adopcion', adoptionRoute)
+//app.use('/proveedores', suppliersRoute)
+//app.use('/servicios', servicesRoute)
 
 }
 

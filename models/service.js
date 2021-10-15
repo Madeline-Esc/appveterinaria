e65f37cf7ac51c2b1a465bf849fa8d7d835a31aa
LuisG00 <gutierrezluis6645@gmail.com>
2021-10-15 16:37:13 -0500

    modificaciones

--- models/service.js
+++ models/service.js
@@ -1,14 +1,14 @@
 const mongoose = require ('mongoose')
 const Schema = mongoose.Schema
 
-const servicesSchema = new Schema({
+const serviceSchema = new Schema({
  nameservice: String,
  duration: {type: String, default: '' },
  price: {type: Number},
-alctualVacunation: [{type: Schema.Types.ObjectId, ref: 'vacunation'}]
+actualVacunation: [{type: Schema.Types.ObjectId, ref: 'vacunation'}]
 },{
   timestamps: true
 })
 
-const Service = mongoose.model('services', servicesSchema)
+const Service = mongoose.model('services', serviceSchema)
 module.exports = Service

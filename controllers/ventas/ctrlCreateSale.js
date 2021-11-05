5e7e03c91c2ae2cb807aea3be96d3a604a3c7519
Madeline-Esc <made_2500@hotmail.com>
2021-11-05 14:24:26 -0600

    operaciones

--- controllers/ventas/ctrlCreateSale.js
+++ controllers/ventas/ctrlCreateSale.js
@@ -3,9 +3,12 @@ const { createSale } = require('../../services/ventas/createSale')
 async function ctrlCreateSale(req, res){
   try{
     const { name, price, description, status } = req.body
-    console.log('Hola desde el controlador')
+
+    var descuento = (price * 0.1)
+    var precioFinal = price - descuento
+
     const sale = await createSale(name, price, description, status)
-    return res.status(200).send(sale)
+    return res.status(200).send({sale, message:`Precio final del producto con descuento del Buen Fin: $${precioFinal}`})
   }catch(error){
     return res.status(error.status || 500).send({message: error.message})
   }
